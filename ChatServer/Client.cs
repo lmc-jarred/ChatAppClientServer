bdf978dbc9e58c7fc3377191b32c312b430278b7
LMC_Jarred <legominecraft369@gmail.com>
2024-11-26 12:04:26 -0500

    Added Client object to ChatServer project. Added list of connected users to ChatServer Program.cs.

---
+++ ChatServer/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatServer
+{
+    internal class Client
+    {
+        public Guid UID { get; set; }
+        public string Username { get; set; }
+        public TcpClient ClientSocket { get; set; }
+
+        public Client(TcpClient client)
+        {
+            ClientSocket = client;
+            UID = Guid.NewGuid();
+
+            Console.WriteLine($"[{DateTime.UtcNow}]: Client has connected with the username: {Username}");
+        }
+    }
+}
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 
@@ -6,15 +7,22 @@ namespace ChatServer
 {
     class Program
     {
+        private static List<Client> _users;
         static TcpListener _listener;
 
         static void Main(string[] args)
         {
+            _users = new List<Client>();
             _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
             _listener.Start();
 
-            TcpClient client = _listener.AcceptTcpClient();
-            Console.WriteLine("Client has connected!");
+            while (true)
+            {
+                Client clientInfo = new Client(_listener.AcceptTcpClient());
+                _users.Add(clientInfo);
+
+                // TODO - Broadcast the connection to everyone on the server
+            }
         }
     }
 }
