d2f1c247ed779246a14c75f3103d14415facf2c9
LMC_Jarred <legominecraft369@gmail.com>
2024-11-26 11:40:22 -0500

    Implemented Server and RelayCommand objects in client. Added MainViewModel and renamed MainWindow to MainView. Bound connect button in MainView.

---
+++ ChatClient/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChatClient.MVVM.Core;
+using ChatClient.Net;
+
+namespace ChatClient.MVVM.ViewModel
+{
+    public class MainViewModel
+    {
+        private Server _server;
+
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+        }
+    }
+}
