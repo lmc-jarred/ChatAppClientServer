2db142f7a9193e0cb856c01f30ec7881bc17902d
LMC_Jarred <legominecraft369@gmail.com>
2024-11-26 11:49:25 -0500

    Implemented accepting a client connection in ChatServer main method.

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static TcpListener _listener;
+
+        static void Main(string[] args)
+        {
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            TcpClient client = _listener.AcceptTcpClient();
+            Console.WriteLine("Client has connected!");
+        }
+    }
+}
